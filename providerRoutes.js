3ab2aa71913b71663d5630c0654a124f3e781270
mohammedyahya <mohammed10yahaya20@gmail.com>
2025-11-25 19:41:49 +0000

    Delete providerRoutes.js

--- providerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Test route
-router.get("/", (req, res) => {
-    res.json({ message: "Provider routes working!" });
-});
-
-module.exports = router;
